01ada8946154ea9e9236c4b7a883e686c1a1a98f
BFostek <bmartins@alunos.utfpr.edu.br>
2023-09-06 23:01:53 -0300

    add axum in the cargo and first boilerplate

--- Dockerfile
+++ Dockerfile
@@ -6,7 +6,7 @@ COPY . .
 
 RUN cargo build --release
 
-FROM debian:slim
+FROM debian
 
 RUN apt-get update && \
     apt-get install -y libssl-dev ca-certificates && \
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{
+    routing::{get, post},
+    http::StatusCode,
+    response::IntoResponse,
+    Json, Router,
+     extract::Path
+};
+use serde::{Deserialize, Serialize};
+use std::net::SocketAddr;
+
+#[tokio::main]
+async fn main() {
+    // initialize tracing
+    tracing_subscriber::fmt::init();
+
+    // build our application with a route
+    let app = Router::new()
+        .route("/", post(root))
+        .route("/", get(root))
+        .route("/:code", get(handle_request));
+
+    // run our app with hyper
+    // `axum::Server` is a re-export of `hyper::Server`
+    let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
+    tracing::debug!("listening on {}", addr);
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+// basic handler that responds with a static string
+async fn root() -> &'static str {
+    "Hello, World!"
+}
+
+async fn handle_request(Path((code,)): Path<(String,)>) -> String {
+    format!("You passed in the code: {}", code)
 }
