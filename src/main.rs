fd3ded6d273c8f71db05e9913b625a46d175e28c
BFostek <bmartins@alunos.utfpr.edu.br>
2023-09-07 15:43:59 -0300

    implement redirect feature

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 use axum::{
     extract::{Path, State},
     http::StatusCode,
-    response::IntoResponse,
+    response::{IntoResponse, Redirect},
     routing::{get, post},
     Router,
+
 };
 use sqlx::postgres::{PgPool, PgPoolOptions};
+use sqlx::Error;
 use std::env;
 use std::net::SocketAddr;
 use std::time::Duration;
@@ -48,18 +50,13 @@ async fn save_url() -> impl IntoResponse {}
 async fn handle_url(
     State(pool): State<PgPool>,
     Path((code,)): Path<(String,)>,
-) -> Result<String , (StatusCode, String)> {
-    match sqlx::query_scalar("SELECT real_url FROM url.urls WHERE hash_url = $1")
+) -> Result<impl IntoResponse, (StatusCode, String)> {
+    let result : Result<String, Error> = sqlx::query_scalar("SELECT real_url FROM url.urls WHERE hash_url = $1")
         .bind(code.to_string())
         .fetch_one(&pool)
-        .await{
-            Ok(url) => Ok(url),
-            Err(_) => {
-                Err((StatusCode::NOT_FOUND, "URL not found".to_string()))
-
-            },
-        }
+        .await;
+    match result {
+        Ok(url) => Ok(Redirect::permanent(&url).into_response()),
+        Err(_) => Err((StatusCode::NOT_FOUND, "URL not found".to_string())),
+    }
 }
-
-
-
